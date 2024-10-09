170666be84ec1f69b7829a8eb78f3379ebc2ad13
Felix <148329742+Felli28@users.noreply.github.com>
2024-10-09 11:45:33 +0200

    Fungerande frontend

--- tests/frontend.spec.ts
+++ tests/frontend.spec.ts
@@ -1,3 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test('Login test', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  await page.locator('input[type="text"]').fill('tester01');
+  await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+});
+
+
+test('Navigate to Clients Page', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+  await page.locator('input[type="text"]').fill('tester01');
+  await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).locator('div').click();
+  await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click()
+
+  await expect(page).toHaveURL('http://localhost:3000/rooms');
+
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
