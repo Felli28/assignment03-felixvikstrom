0f2574a888fdc871e508eccacf7e0a51aef2ee32
Felix <148329742+Felli28@users.noreply.github.com>
2024-10-11 14:25:09 +0200

    Ny frontend

--- tests/frontend.spec.ts
+++ tests/frontend.spec.ts
@@ -3,24 +3,27 @@ import { test, expect } from '@playwright/test';
 const username = process.env.TEST_USERNAME;
 const password = process.env.TEST_PASSWORD;
 
-test('1.Login test', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
-  await page.locator('input[type="text"]').fill(username);
-  await page.locator('input[type="password"]').fill(password);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page).toHaveURL('http://localhost:3000/');
-});
-
-test('2.Navigate to Clients Page', async ({ page }) => {
+test('Add client on the clients page', async ({ page }) => {
   await page.goto('http://localhost:3000/login');
   await page.locator('input[type="text"]').fill(username);
   await page.locator('input[type="password"]').fill(password);
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page).toHaveURL('http://localhost:3000/');
 
-  await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).locator('div').click();
-  await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
+  await page.locator('div').filter({ hasText: /^ClientsNumber: \d+View$/ }).getByRole('link').click();
+  await expect(page).toHaveURL('http://localhost:3000/clients');
+  
+  await page.getByRole('link', { name: 'Create Client' }).click();
+  await page.waitForURL('http://localhost:3000/client/new');
 
-  await expect(page).toHaveURL('http://localhost:3000/rooms');
+  await page.locator('input[type="text"]').nth(0).fill('Adam Jalla');
+  await page.locator('input[type="email"]').fill('adam.jalla@example.com');
+  await page.locator('input[type="text"]').nth(1).fill('070 123 4567');
+
+  const saveButton = page.locator('a.btn.blue:has-text("Save")');
+  await saveButton.click();
+  
+  await expect(page).toHaveURL('http://localhost:3000/clients');
+  await expect(page.locator('text=Adam Jalla')).toBeVisible();
 });
 
